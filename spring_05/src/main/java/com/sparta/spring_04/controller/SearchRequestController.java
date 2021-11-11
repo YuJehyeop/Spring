49efe51d27f49822c24c4e98dcf267884c3e37e6
Yu_Jehyeop <fbwpguq@gmail.com>
2021-11-12 03:22:40 +0900

    스프링-3_04_미완성_에러제거

---
+++ spring_05/src/main/java/com/sparta/spring_04/controller/SearchRequestController.java
@@ -0,0 +1,23 @@
+package com.sparta.spring_04.controller;
+
+import com.sparta.spring_04.util.NaverShopSearch;
+import com.sparta.spring_04.dto.ItemDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController // JSON으로 응답함을 선언합니다.
+@RequiredArgsConstructor
+public class SearchRequestController {
+
+    private final NaverShopSearch naverShopSearch;
+
+    @GetMapping("/api/search")
+    public List<ItemDto> getItems(@RequestParam String query) {
+        String resultString = naverShopSearch.search(query);
+        return naverShopSearch.fromJSONtoItems(resultString);
+    }
+}
---
+++ spring_05/src/main/java/com/sparta/spring_04/dto/ProductMypriceRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.spring_04.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
