c3b937b344602a4f6e467bc815ee3d9a5d7f3a9d
Yu_Jehyeop <fbwpguq@gmail.com>
2021-11-12 02:22:10 +0900

    스프링-2_13_중복제거

--- sprint_04/src/main/java/com/sparta/spring_04/ProductController.java
+++ sprint_04/src/main/java/com/sparta/spring_04/ProductController.java
@@ -1,6 +1,5 @@
 package com.sparta.spring_04;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
@@ -8,10 +7,17 @@ import java.util.List;
 
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
+    // 멤버 변수 선언
+    private final ProductService productService;
+
+    public ProductController() {
+        // 멤버 변수 생성
+        productService = new ProductService();
+    }
+
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
     public List<Product> getProducts() throws SQLException {
-        ProductService productService = new ProductService();
         List<Product> products = productService.getProducts();
         // 응답 보내기
         return products;
@@ -20,7 +26,6 @@ public class ProductController {
     // 신규 상품 등록
     @PostMapping("/api/products")
     public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
         Product product = productService.createProduct(requestDto);
         // 응답 보내기
         return product;
@@ -29,7 +34,6 @@ public class ProductController {
     // 설정 가격 변경
     @PutMapping("/api/products/{id}")
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
         Product product = productService.updateProduct(id, requestDto);
         return product.getId();
     }
