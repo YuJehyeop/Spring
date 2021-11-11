4a17dfa40f4ecb7ba040c333d5fb538d1f1add7c
Yu_Jehyeop <fbwpguq@gmail.com>
2021-11-12 03:00:07 +0900

    스프링-2_17_폴더정리

---
+++ sprint_04/src/main/java/com/sparta/spring_04/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.spring_04.repository;
+
+import com.sparta.spring_04.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByUserId(Long userId);
+}
