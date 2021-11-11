43936f62ffd509a3b92150af1eaf83baa508bada
Yu_Jehyeop <fbwpguq@gmail.com>
2021-11-11 22:49:58 +0900

    스프링-2_06_나만의셀렉샵API

--- sprint_04/src/main/java/com/sparta/spring_04/Spring04Application.java
+++ sprint_04/src/main/java/com/sparta/spring_04/Spring04Application.java
@@ -2,10 +2,13 @@ package com.sparta.spring_04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
+@ServletComponentScan
 public class Spring04Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Spring04Application.class, args);
 	}
