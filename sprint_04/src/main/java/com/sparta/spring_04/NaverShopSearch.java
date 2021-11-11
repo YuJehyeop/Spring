97fd3923a8a5a6178df62b5ea7f874ff7f042fac
Yu_Jehyeop <fbwpguq@gmail.com>
2021-11-11 22:34:48 +0900

    스프링-2_06_15

---
+++ sprint_04/src/main/java/com/sparta/spring_04/NaverShopSearch.java
@@ -0,0 +1,53 @@
+package com.sparta.spring_04;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component // 스프링 IoC 에 빈으로 등록
+@PropertySource("classpath:config.properties")
+public class NaverShopSearch {
+
+    @Value("${X-Naver-Client-Id}")
+    private String xNaverClientId;
+
+    @Value("${X-Naver-Client-Secret}")
+    private String xNaverClientSeceret;
+
+    public String search(String query) {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Naver-Client-Id", xNaverClientId);
+        headers.add("X-Naver-Client-Secret", xNaverClientSeceret);
+        String body = "";
+
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=" + query, HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        String response = responseEntity.getBody();
+        System.out.println("Response status: " + status);
+        System.out.println(response);
+
+        return response;
+    }
+
+    public List<ItemDto> fromJSONtoItems(String result) {
+        JSONObject rjson = new JSONObject(result);
+        JSONArray items  = rjson.getJSONArray("items");
+        List<ItemDto> ret = new ArrayList<>();
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = (JSONObject) items.get(i);
+            ItemDto itemDto = new ItemDto(itemJson);
+            ret.add(itemDto);
+        }
+        return ret;
+    }
+}
---
+++ sprint_04/src/main/java/com/sparta/spring_04/Spring04Application.java
@@ -0,0 +1,12 @@
+package com.sparta.spring_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring04Application.class, args);
+	}
+}
